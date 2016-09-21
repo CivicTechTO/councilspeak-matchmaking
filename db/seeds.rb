87107e5db981f7f3d0ab51fecbbaf779c9a55bfe
Patrick Connolly <patrick.c.connolly@gmail.com>
2016-09-21 02:04:11 -0400

    Added agenda_item seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'csv'
+
+puts "Seeding Agenda Item records..."
+csv_file = File.join Rails.root, 'db/data/agenda_items.csv'
+CSV.foreach(csv_file, headers: true) do |row|
+  data = {
+    title: row['title'],
+    meeting_date: Date.parse(row['meeting_date']),
+    identifier: row['identifier'],
+    organization: row['organization'],
+  }
+  AgendaItem.create data
+end
