7f8bba9ff265f311f9d4374f505a1805db17593f
Patrick Connolly <patrick.c.connolly@gmail.com>
2016-09-21 17:13:17 -0400

    Fixed ambiguous associations for speakings/presentations.

--- app/models/deputation.rb
+++ app/models/deputation.rb
@@ -1,7 +1,7 @@
 class Deputation < ApplicationRecord
   validates :video_url, presence: true
 
-  belongs_to :speaker, class_name: "Speaker"
-  belongs_to :presenter, class_name: "Presenter", optional: true
+  belongs_to :speaker, foreign_type: "Deputation", class_name: "Speaker"
+  belongs_to :presenter, foreign_type: "Deputation", class_name: "Presenter", optional: true
   belongs_to :agenda_item
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,6 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true
 
-  has_many :deputations
+  has_many :speakings, class_name: "Deputation", foreign_key: "speaker_id"
+  has_many :presentations, class_name: "Deputation", foreign_key: "presenter_id"
 end
