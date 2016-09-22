88c3d91ecf4f7f0bbff631b7d6890be6a48b3e26
Patrick Connolly <patrick.c.connolly@gmail.com>
2016-09-22 04:39:07 -0400

    Added note about checking youtube video duration.

--- app/controllers/deputations_controller.rb
+++ app/controllers/deputations_controller.rb
@@ -1,4 +1,7 @@
 class DeputationsController < ApplicationController
   def new
   end
+
+  # TODO: Confirm YouTube video duration is under 2 minutes (plus a little forgiveness).
+  # part:contentDetails and parse duration. Needs auth.
 end
