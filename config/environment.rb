0ce7ec31187ba18262b8e8ecd647867f1d1c9cef
Patrick Connolly <patrick.c.connolly@gmail.com>
2016-09-25 13:53:30 -0400

    Moved stmp config to new location for troubleshooting.

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,12 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+Rails.application.config.action_mailer.smtp_settings = {
+  :address => "smtp.sendgrid.net",
+  :port => 25,
+  :domain => "example.com",
+  :authentication => :plain,
+  :user_name => ENV['SENDGRID_USERNAME'],
+  :password => ENV['SENDGRID_PASSWORD'],
+}
